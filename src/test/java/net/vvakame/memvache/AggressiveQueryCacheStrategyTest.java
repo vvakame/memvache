a1a25105d9168ce2edde7907cc5bf4ba04ab780d
vvakame <vvakame+dev@gmail.com>
2012-12-02 00:04:17 +0900

    Added test for AggressiveQueryCacheStrategy

--- src/test/java/net/vvakame/memvache/AggressiveQueryCacheStrategyTest.java
+++ src/test/java/net/vvakame/memvache/AggressiveQueryCacheStrategyTest.java
@@ -1,14 +1,19 @@
 package net.vvakame.memvache;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.slim3.datastore.Datastore;
 import org.slim3.datastore.EntityQuery;
 import org.slim3.datastore.KindlessQuery;
+import org.slim3.memcache.Memcache;
 import org.slim3.tester.ControllerTestCase;
 
+import com.google.appengine.api.NamespaceManager;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 
 import static org.hamcrest.CoreMatchers.*;
 
@@ -20,7 +25,9 @@ import static org.junit.Assert.*;
  */
 public class AggressiveQueryCacheStrategyTest extends ControllerTestCase {
 
-	DebugDelegate debugDelegate;
+	MemvacheDelegate memvacheDelegate;
+
+	RpcCounterDelegate countDelegate;
 
 
 	/**
@@ -28,25 +35,134 @@ public class AggressiveQueryCacheStrategyTest extends ControllerTestCase {
 	 * @author vvakame
 	 */
 	@Test
-	public void run_RPCs() {
+	public void queryCache() {
+		assertThat(Memcache.statistics().getItemCount(), is(0L));
+
+		Datastore.query("hoge").asEntityList().size();
+
+		assertThat("Queryのキャッシュが作成された", Memcache.statistics().getItemCount(), is(1L));
+
+		final Map<String, Integer> countMap = countDelegate.countMap;
+		// {memcache@Stats=2, datastore_v3@RunQuery=1, memcache@Get=3, memcache@Set=1}
+		assertThat("Queryの回数", countMap.get("datastore_v3@RunQuery"), is(1));
+
+		Datastore.query("hoge").asEntityList().size();
+
+		assertThat("増えてない", countMap.get("datastore_v3@RunQuery"), is(1));
+	}
+
+	/**
+	 * テストケース。
+	 * @author vvakame
+	 */
+	@Test
+	public void expireQueryCache() {
+		queryCache();
+		final Map<String, Integer> countMap = countDelegate.countMap;
+		assertThat(countMap.get("datastore_v3@RunQuery"), is(1));
+
+		Datastore.put(new Entity("hoge"));
+		assertThat("increment", Memcache.statistics().getItemCount(), is(2L));
+
+		Datastore.query("hoge").asEntityList().size();
+
+		assertThat("増えた", countMap.get("datastore_v3@RunQuery"), is(2));
+	}
+
+	/**
+	 * テストケース。
+	 * @author vvakame
+	 */
+	@Test
+	public void expireQueryCacheByAncester() {
+		queryCache();
+		final Map<String, Integer> countMap = countDelegate.countMap;
+		assertThat(countMap.get("datastore_v3@RunQuery"), is(1));
+
 		{
-			Entity entity = new Entity("hoge", 1);
-			entity.setProperty("v", "value1");
-			Datastore.put(entity);
+			Key parent = Datastore.createKey("hoge", 1);
+			Key child = Datastore.createKey(parent, "foo", 1000);
+			Datastore.put(new Entity(child));
 		}
+		Datastore.query("hoge").asEntityList().size();
+		assertThat("増えてない", countMap.get("datastore_v3@RunQuery"), is(1));
+
 		{
-			Entity entity = new Entity("hoge", 2);
-			entity.setProperty("v", "value2");
-			Datastore.put(entity);
-		}
-		{
-			Entity entity = new Entity("hoge", 3);
-			entity.setProperty("v", "value3");
-			Datastore.put(entity);
+			Key parent = Datastore.createKey("foo", 1);
+			Key child = Datastore.createKey(parent, "hoge", 1000);
+			Datastore.put(new Entity(child));
 		}
 
-		List<Entity> entities = Datastore.query("hoge").asEntityList();
-		assertThat(entities.size(), is(3));
+		Datastore.query("hoge").asEntityList().size();
+		assertThat("増えた", countMap.get("datastore_v3@RunQuery"), is(2));
+	}
+
+	/**
+	 * テストケース。
+	 * @author vvakame
+	 */
+	@Test
+	public void notExpireQueryByOtherNamespace() {
+		queryCache();
+		final Map<String, Integer> countMap = countDelegate.countMap;
+		assertThat(countMap.get("datastore_v3@RunQuery"), is(1));
+
+		NamespaceManager.set("other");
+		Datastore.put(new Entity("hoge"));
+
+		NamespaceManager.set(null);
+
+		Datastore.query("hoge").asEntityList().size();
+		assertThat("増えない", countMap.get("datastore_v3@RunQuery"), is(1));
+	}
+
+	/**
+	 * テストケース。
+	 * @author vvakame
+	 */
+	@Test
+	public void expireQueryPutMultiKind() {
+		Datastore.query("hoge").asEntityList().size();
+		Datastore.query("fuga").asEntityList().size();
+
+		final Map<String, Integer> countMap = countDelegate.countMap;
+		assertThat(countMap.get("datastore_v3@RunQuery"), is(2));
+		assertThat("query cache 2", Memcache.statistics().getItemCount(), is(2L));
+
+		List<Entity> entities = new ArrayList<Entity>();
+		entities.add(new Entity("hoge"));
+		entities.add(new Entity("fuga"));
+		entities.add(new Entity("hige"));
+		Datastore.put(entities);
+		assertThat("3Kind inc", Memcache.statistics().getItemCount(), is(5L));
+
+		Datastore.query("hoge").asEntityList().size();
+		Datastore.query("fuga").asEntityList().size();
+
+		assertThat(countMap.get("datastore_v3@RunQuery"), is(4));
+		assertThat("query cache 2", Memcache.statistics().getItemCount(), is(7L));
+	}
+
+	/**
+	 * テストケース。
+	 * @author vvakame
+	 */
+	@Test
+	public void ignoreKind() {
+		Settings.getInstance().ignoreKinds.clear();
+
+		Settings.getInstance().ignoreKinds.add("hoge");
+
+		assertThat(Memcache.statistics().getItemCount(), is(0L));
+
+		Datastore.put(new Entity("hoge"));
+		Datastore.query("hoge").asEntityList().size();
+		assertThat(Memcache.statistics().getItemCount(), is(0L));
+
+		Datastore.query("fuga").asEntityList().size();
+		assertThat(Memcache.statistics().getItemCount(), is(1L));
+
+		Settings.getInstance().ignoreKinds.clear();
 	}
 
 	/**
@@ -93,13 +209,18 @@ public class AggressiveQueryCacheStrategyTest extends ControllerTestCase {
 	public void setUp() throws Exception {
 		super.setUp();
 
-		debugDelegate = DebugDelegate.install();
-		debugDelegate.setVisitor(new AggressiveQueryCacheStrategy());
+		// かならず RpcCounterDelegate が最初
+		countDelegate = RpcCounterDelegate.install();
+
+		memvacheDelegate = MemvacheDelegate.install();
+		memvacheDelegate.strategies.clear();
+		memvacheDelegate.strategies.add(new AggressiveQueryCacheStrategy());
 	}
 
 	@Override
 	public void tearDown() throws Exception {
-		debugDelegate.uninstall();
+		memvacheDelegate.uninstall();
+		countDelegate.uninstall();
 
 		super.tearDown();
 	}
